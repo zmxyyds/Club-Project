e2870c1b780d689b594947a69f0c7cc05817d0db
Boring coils <137899864+zmxyyds@users.noreply.github.com>
2025-11-15 22:45:37 +0800

    feat(玩家系统): 添加Buff组件并完善生命值系统
    - 新增Buff.gd脚本作为玩家Buff系统基础
    - 在health.gd中添加对Buff节点的引用和父节点处理
    - 修改world.tscn场景，为玩家节点添加Health和Buff组件
    - 更新.gitignore文件，添加doc/目录忽略
    - 移除health.gd中的调试打印语句

--- club-project/Scripts/health.gd
+++ club-project/Scripts/health.gd
@@ -1,16 +1,27 @@
 extends Node
 
+##显然，这是生命值组件的父节点。[br]
+##也就是说，比如我要给[玩家]添加[生命值组件]，那么[生命值组件]所在的节点应当是[玩家]节点的第一级子节点
+@onready var father_node = get_parent()
+
+##[b][i][u][color=green]答案显而易见了[/color][/u][/i][/b]
+##这个节点是生命值组件所附对象上的buff组件。考虑到有些对象可能不会有buff………………………………
+@onready var buff_node = get_node("../Buff")
+
 ## [i]基础[/i]最大生命值 [br]
 ## 还有[i]实际[/i]最大值,详见[actual_max_health]。[br]
 ## 使用函数[method set_max_health]可以修改基础最大生命值
 @export var max_health: float = 100.0
+
 ## [i]实际[/i]最大生命值 [br]
 ## 这个值是对象实实在在的最大生命值表现。
 var actual_max_health: float = max_health
+
 ## 实际当前生命值 [br]
 ## 请不要使用脚本直接调用这个变量，可能导致生命值异常等安全性问题 [br] 
 ## 
 @export var current_health: float = 0.0
+
 ## @experimental:目前而言，这个函数对当前生命值的操作还有待改进
 ## 设置基础最大生命值 [br]
 ## 参数[value] ：设置的值[br] 
@@ -39,6 +50,7 @@ func set_max_health(value: float, set_max_health_mode: String = "set") -> void:
 ## 返回值：当前生命值占基础最大生命值的百分比 [br]
 func get_health_percentage() -> float:
 	return current_health / actual_max_health
+
 ## 治疗 [br]
 ## 参数[value] ：治疗的值[br] 
 ## 治疗后，如果当前生命值超过实际最大生命值，会自动设置为实际最大生命值。[br]
@@ -52,12 +64,17 @@ func treat(value: float)->void:
 
 
 
+
 func _ready() -> void:
 	if current_health == 0:
 		current_health = max_health
 	elif current_health > max_health:
 		current_health = max_health
-	print("最大生命值：", max_health)
-	print("当前生命值：", current_health)
-	
+	#print("最大生命值：", max_health)
+	#print("当前生命值：", current_health)
+	#print("实际最大生命值：", actual_max_health)
+	#print("当前生命值百分比：", get_health_percentage())
+	print("buff节点：", buff_node)
+	#print("父节点：", father_node)
+	#print(father_node.get_movement_vector())
 	
